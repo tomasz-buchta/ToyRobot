1e898e1163e8fc427cf05291721245c699c95e55
Tomasz Buchta <tomasz.buchta@hotmail.com>
2015-09-18 13:45:37 +0200

    Added rotation ability

--- lib/robot.rb
+++ lib/robot.rb
@@ -1,4 +1,28 @@
+require 'pry'
 module TOYROBOT
   class Robot
+    attr_reader :position
+    def initialize
+      # @directions = RingBuffer.new(4)
+      @directions = %w[NORTH EAST SOUTH WEST]
+    end
+    def place(position)
+      @position = position
+    end
+
+    def report
+      "#{@position.x} #{@position.y} #{@position.heading}"
+    end
+
+    def rotate(direction)
+      case direction
+        when 'RIGHT'
+          @position[:heading] = @directions[@directions.find_index(@position[:heading]) + 1]
+        when 'LEFT'
+          @position[:heading] = @directions[@directions.find_index(@position[:heading]) - 1]
+        else
+
+      end
+    end
   end
 end
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -1,9 +1,26 @@
 require 'rspec'
 
-describe 'My behaviour' do
+describe TOYROBOT::Robot do
+  before(:each) do
+    @robot = TOYROBOT::Robot.new
+  end
+  describe '#place' do
+    it 'should place the robot' do
+      @robot.place({x:0,y:0,heading:'NORTH'})
+      expect(@robot.position).to eq({x:0,y:0,heading:'NORTH'})
+    end
+  end
 
-  it 'should do something' do
+  describe '#rotate' do
+    before(:each) do
+      @robot.place({x:0,y:0,heading:'NORTH'})
+    end
+    it 'should rotate RIGHT from NORTH to EAST' do
+      expect{@robot.rotate('RIGHT')}.to change{@robot.position[:heading]}.from('NORTH').to('EAST')
+    end
 
-    true.should == false
+    it 'should rotate LEFT from NORTH to WEST' do
+      expect{@robot.rotate('LEFT')}.to change{@robot.position[:heading]}.from('NORTH').to('WEST')
+    end
   end
 end
