d3b534de19f14ef6bdf5c75174182eafc2b9a113
Tomasz Buchta <tomasz.buchta@polcode.net>
2015-09-21 12:54:46 +0200

    Test refactor to use let

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -4,42 +4,57 @@ describe TOYROBOT::Main do
   describe '#parse_cmd' do
     let(:main) {TOYROBOT::Main.new}
 
-    describe 'when PLACE' do
-      before(:each) {@robot = main.instance_variable_get(:@robot)}
-      it 'should call the place with appropriate arguments' do
-        expect(@robot).to receive(:place).with({x:0,y:0,heading:'NORTH'})
-        main.parse_cmd('PLACE 0,0,NORTH')
-      end
-      describe 'without valid params' do
-        it 'should not call the place' do
-          expect(@robot).to_not receive(:place)
-          main.parse_cmd('PLACE 0,NORTH')
+    describe @robot do
+
+      let(:robot) {main.instance_variable_get(:@robot)}
+
+      describe 'when PLACE' do
+
+        it 'should call the place with appropriate arguments' do
+          expect(robot).to receive(:place).with({x:0,y:0,heading:'NORTH'})
+          main.parse_cmd('PLACE 0,0,NORTH')
         end
+
+        describe 'without valid params' do
+
+          it 'should not call the place' do
+            expect(robot).to_not receive(:place)
+            main.parse_cmd('PLACE 0,NORTH')
+          end
+
+        end
+
+      end
+      describe 'when ROTATE' do
+
+        it 'should call the rotate with RIGHT' do
+          expect(robot).to receive(:rotate).with('RIGHT')
+          main.parse_cmd('RIGHT')
+        end
+
+        it 'should call the rotate with LEFT' do
+          expect(robot).to receive(:rotate).with('LEFT')
+          main.parse_cmd('LEFT')
+        end
+
+        it 'should not call rotate' do
+          expect(robot).to_not receive(:rotate)
+          main.parse_cmd('CENTER')
+        end
+
+      end
+
+      describe 'when MOVE' do
+
+        it 'should call move' do
+          expect(robot).to receive(:move)
+          main.parse_cmd('MOVE')
+        end
+
       end
     end
 
-    describe 'when ROTATE' do
-      it 'should call the rotate with RIGHT' do
-        expect(main.instance_variable_get(:@robot)).to receive(:rotate).with('RIGHT')
-        main.parse_cmd('RIGHT')
-      end
 
-      it 'should call the rotate with LEFT' do
-        expect(main.instance_variable_get(:@robot)).to receive(:rotate).with('LEFT')
-        main.parse_cmd('LEFT')
-      end
 
-      it 'should not call rotate' do
-        expect(main.instance_variable_get(:@robot)).to_not receive(:rotate)
-        main.parse_cmd('CENTER')
-      end
-    end
-
-    describe 'when MOVE' do
-      it 'should call move' do
-        expect(main.instance_variable_get(:@robot)).to receive(:move)
-        main.parse_cmd('MOVE')
-      end
-    end
   end
 end
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -1,87 +1,87 @@
 require 'rspec'
 
 describe TOYROBOT::Robot do
-  before(:each) do
-    @robot = TOYROBOT::Robot.new
-  end
+
+  let(:robot) {TOYROBOT::Robot.new}
+
   describe '#place' do
     it 'should place the robot' do
-      @robot.place({x:0,y:0,heading:'NORTH'})
-      expect(@robot.position).to eq({x:0,y:0,heading:'NORTH'})
+      robot.place({x:0,y:0,heading:'NORTH'})
+      expect(robot.position).to eq({x:0,y:0,heading:'NORTH'})
     end
   end
 
   describe '#rotate' do
     before(:each) do
-      @robot.place({x:0,y:0,heading:'NORTH'})
+      robot.place({x:0,y:0,heading:'NORTH'})
     end
     it 'should rotate RIGHT from NORTH to EAST' do
-      expect{@robot.rotate('RIGHT')}.to change{@robot.position[:heading]}.from('NORTH').to('EAST')
+      expect{robot.rotate('RIGHT')}.to change{robot.position[:heading]}.from('NORTH').to('EAST')
     end
 
     it 'should rotate LEFT from NORTH to WEST' do
-      expect{@robot.rotate('LEFT')}.to change{@robot.position[:heading]}.from('NORTH').to('WEST')
+      expect{robot.rotate('LEFT')}.to change{robot.position[:heading]}.from('NORTH').to('WEST')
     end
   end
 
   describe '#report' do
     before(:each) do
-      @robot.place({x:0,y:0,heading:'NORTH'})
+      robot.place({x:0,y:0,heading:'NORTH'})
     end
     it 'should report correct position' do
-      expect(@robot.report).to eq('0 0 NORTH')
+      expect(robot.report).to eq('0 0 NORTH')
     end
   end
 
   describe '#move' do
     it 'should move NORTH' do
-      @robot.place({x:1,y:1,heading:'NORTH'})
-      @robot.move
-      expect(@robot.position).to eq({x:1,y:2,heading:'NORTH'})
+      robot.place({x:1,y:1,heading:'NORTH'})
+      robot.move
+      expect(robot.position).to eq({x:1,y:2,heading:'NORTH'})
     end
 
     it 'should move SOUTH' do
-      @robot.place({x:1,y:1,heading:'SOUTH'})
-      @robot.move
-      expect(@robot.position).to eq({x:1,y:0,heading:'SOUTH'})
+      robot.place({x:1,y:1,heading:'SOUTH'})
+      robot.move
+      expect(robot.position).to eq({x:1,y:0,heading:'SOUTH'})
     end
 
     it 'should move EAST' do
-      @robot.place({x:1,y:1,heading:'EAST'})
-      @robot.move
-      expect(@robot.position).to eq({x:2,y:1,heading:'EAST'})
+      robot.place({x:1,y:1,heading:'EAST'})
+      robot.move
+      expect(robot.position).to eq({x:2,y:1,heading:'EAST'})
     end
 
     it 'should move WEST' do
-      @robot.place({x:1,y:1,heading:'WEST'})
-      @robot.move
-      expect(@robot.position).to eq({x:0,y:1,heading:'WEST'})
+      robot.place({x:1,y:1,heading:'WEST'})
+      robot.move
+      expect(robot.position).to eq({x:0,y:1,heading:'WEST'})
     end
     describe 'private' do
       describe '#in_bounds?' do
 
         it 'should be true for valid lower bound position' do
-          expect(@robot.send(:in_bounds?,{x:0,y:0,heading:'NORTH'})).to be(true)
+          expect(robot.send(:in_bounds?,{x:0,y:0,heading:'NORTH'})).to be(true)
         end
 
         it 'should be true for valid upper bound position' do
-          expect(@robot.send(:in_bounds?,{x:4,y:4,heading:'NORTH'})).to be(true)
+          expect(robot.send(:in_bounds?,{x:4,y:4,heading:'NORTH'})).to be(true)
         end
 
         it 'should be false for x < 0' do
-          expect(@robot.send(:in_bounds?,{x:-1,y:2,heading:'NORTH'})).to be(false)
+          expect(robot.send(:in_bounds?,{x:-1,y:2,heading:'NORTH'})).to be(false)
         end
 
         it 'should be false for x > 4' do
-          expect(@robot.send(:in_bounds?,{x:5,y:2,heading:'NORTH'})).to be(false)
+          expect(robot.send(:in_bounds?,{x:5,y:2,heading:'NORTH'})).to be(false)
         end
 
         it 'should be false for y < 0' do
-          expect(@robot.send(:in_bounds?,{x:1,y:-1,heading:'NORTH'})).to be(false)
+          expect(robot.send(:in_bounds?,{x:1,y:-1,heading:'NORTH'})).to be(false)
         end
 
         it 'should be false for x < 4' do
-          expect(@robot.send(:in_bounds?,{x:1,y:5,heading:'NORTH'})).to be(false)
+          expect(robot.send(:in_bounds?,{x:1,y:5,heading:'NORTH'})).to be(false)
         end
 
       end
